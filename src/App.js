de0493d02420ad72c0adb1960103e8a744abf271
i-metlyuk <igor.m@core-tech.ru>
2022-01-10 11:57:24 +0300

    finishing custom hooks

--- src/App.js
+++ src/App.js
@@ -1,71 +1,82 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component, useRef, useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-class Form extends Component {
-    state = {
-        advOpen: false
+function useInputWithValidate(initalValue) {
+    const [value, setValue] = useState(initalValue);
+
+    const onChange = event => {
+        setValue(event.target.value);
     }
 
-    componentDidMount() {
-        setTimeout(this.handleClick, 3000)
+    const validateInput = () => {
+        return (value.search(/\d/) >= 0)
     }
 
-    handleClick = () => {
-        this.setState(({advOpen}) => ({
-            advOpen: !advOpen
-        }))
+    return {
+        value: value,
+        onChange: onChange,
+        validateInput: validateInput
+    }
+}
+
+const Form = () => {
+    const input = useInputWithValidate('');
+    const textArea = useInputWithValidate('');
+
+    const color = input.validateInput() ? 'text-danger' : null;
+
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <input
+                        value={`${input.value} / ${textArea.value}`}
+                        type='text'
+                        className='form-control'
+                        readOnly>
+                    </input>
+                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+                    <input
+                        value={input.value}
+                        onChange={input.onChange}
+                        type="email"
+                        className={`form-control ${color}`}
+                        id="exampleFormControlInput1"
+                        placeholder="name@example.com"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea
+                        onChange={textArea.onChange}
+                        value={textArea.value}
+                        className="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="3">
+                    </textarea>
+                </div>
+            </form>
+        </Container>
+    )
+
+}
+
+class TextInput extends Component {
+    doSmth = () => {
+        console.log('smth')
     }
 
     render() {
         return (
-            <Container>
-                <form 
-                    onClick={this.handleClick}
-                    className="w-50 border mt-5 p-3 m-auto"
-                    style={{'overflow': 'hidden', 
-                        'position': 'relative'}}>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input  type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    {
-                        this.state.advOpen ? 
-                        <Portal>
-                            <Msg></Msg>
-                        </Portal> : null
-                    }
-                    
-                </form>
-            </Container>
-        )
+            <input
+                type="email"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+            />
+        );
     }
-}
-
-const Portal = (props) => {
-    const node = document.createElement('div');
-    document.body.appendChild(node);
-
-    return ReactDOM.createPortal(props.children, node);
-}
-
-const Msg = () => {
-    return (
-        <div 
-            style={{'width': '500px', 
-                    'height': '150px', 
-                    'backgroundColor': 'red', 
-                    'position': 'absolute', 
-                    'right': '0', 
-                    'bottom': '0'}}>
-            Hello
-        </div>
-    );
 }
 
 function App() {
